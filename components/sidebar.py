af38685ccf3a78056db24fe1299888e437611b80
brenorodrigues77 <breno7rodrigues@gmail.com>
2024-12-03 22:29:43 -0300

    Update  sidebar.py
    Continuação das opções do modal de receita, inserindo novas funcionalidades para filtrar, modificar, inserir, excluir os dados.

--- components/sidebar.py
+++ components/sidebar.py
@@ -21,61 +21,107 @@ layout = dbc.Card([
                     html.P('SESACRE', className='text-info'),
                     html.Hr(),
 
-        #perfil
-        dbc.Button(id='perfil',
-                children=[html.Img(src='./assets/icon.png', id='icone_perfil', alt='avatar', className='perfil_avatar')],
-                style={'background-color': 'transparent', 'border-color': 'transparent',}),
-                
-        #botoes para modais
-        dbc.Col([
+            #perfil
+            dbc.Button(id='perfil',
+                    children=[html.Img(src='./assets/icon.png', id='icone_perfil', alt='avatar', className='perfil_avatar')],
+                    style={'background-color': 'transparent', 'border-color': 'transparent',}),
+                    
+            #botoes para modais
             dbc.Col([
-                dbc.Button(color='success', id='botao-receita', children=['Receitas'])
-                ], width=4, style={'margin': '5px'}),
-            dbc.Col(
-                [dbc.Button(color='danger', id='botao-medicamentos', children=['Medicamentos'])
-                ], width=4, style={'margin': '5px' }),
-        ]),
+                dbc.Col([
+                    dbc.Button(color='success', id='botao-receita', children=['Receitas'])
+                    ], width=4, style={'margin': '5px'}),
+                dbc.Col(
+                    [dbc.Button(color='danger', id='botao-medicamentos', children=['Medicamentos'])
+                    ], width=4, style={'margin': '5px' }),
+                ]),
         
-        #modal da opcao1
-        dbc.Modal([
-            dbc.ModalHeader(dbc.ModalTitle('Receitas')),
-            dbc.ModalBody([
-                dbc.Row([
-                    dbc.Col([
-                        dbc.Label('Descrição'),
-                        dbc.Input(placeholder='Descrição', id='descricao-modal1'),
-                        
-                    ], width=6),
-                    dbc.Col([
-                        dbc.Label('Valor'),
-                        dbc.Input(placeholder='Valor', id='valor-modal1')
-                    ],width=6)
-                ])
+            #modal da receita
+            dbc.Modal([
+                dbc.ModalHeader(dbc.ModalTitle('Receitas')),
+                dbc.ModalBody([
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.Label('Descrição'),
+                            dbc.Input(placeholder='Descrição', id='descricao-modal1'),
+                            
+                        ], width=6),
+                        dbc.Col([
+                            dbc.Label('Valor'),
+                            dbc.Input(placeholder='Valor', id='valor-modal1', value="")
+                        ], width=3),
+                    ]),
 
-            ]),
-        ], id='modal-botao1'),
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.Label('Data:'),
+                            dcc.DatePickerSingle(id="data-modal1",
+                            min_date_allowed=date(2024,4,1),
+                            max_date_allowed=date(2029,12,31),
+                            date=datetime.today(),
+                            style={'width': '100%'}
+                            ),
+                        ],width=4 , style={'margin-top': '10px'}),
 
-        dbc.Modal([
-            dbc.ModalHeader(dbc.ModalTitle('Medicamentos')),
-            dbc.ModalBody([
+                        dbc.Col([
+                            dbc.Label('Outros'),
+                            dbc.Checklist(
+                                options=[],
+                                value=[],
+                                id='outros-receita',
+                                switch=True
+                            ),
+                        ],width=4 , style={'margin-top': '10px'}),
+
+                        dbc.Col([
+                            html.Label('Categoria da Receita'),
+                            dbc.Select(
+                                id='selecao-receita',
+                                options=[],
+                                value=[]
+                                ),
+                        ],width=4 , style={'margin-top': '10px'})
+                    ]),
+
+                    dbc.Row([
+                        dbc.Accordion([
+                            dbc.AccordionItem(children=[
+                                dbc.Row([
+                                    dbc.Col([
+                                       html.Legend('Adicionar Categoria', style={'color': 'green'}),
+                                       dbc.Input()
+                                    ])
+                                ])
+                            ])
+                        ])
+                    ])
+                ]),
+            ], id='modal-botao1'),
+
+            #modal dos medicamentos
+            dbc.Modal([
+                dbc.ModalHeader(dbc.ModalTitle('Medicamentos')),
+                dbc.ModalBody([
+                
+                ]),
+            ], id='modal-botao2'),
             
-            ]),
-        ], id='modal-botao2'),
-        
-        html.Hr(),
+            html.Hr(),
 
-        #seção de navegação
-        dbc.Nav([
-            dbc.NavLink('Dashboard', href='/dashboards', active='exact'),
-            dbc.NavLink('Medicamentos', href='/medicamentos', active='exact'),
-            dbc.NavLink('Receitas', href='/receitas', active='exact'),
-        ], vertical=True, pills=True, id='nav_bottons', style={'margin-bottom': '50px'}),
-    ], id='sidebar_completa')
-], style={'margin-top': '20px'})
+            #seção de navegação
+            dbc.Nav([
+                dbc.NavLink('Dashboard', href='/dashboards', active='exact'),
+                dbc.NavLink('Medicamentos', href='/medicamentos', active='exact'),
+                dbc.NavLink('Receitas', href='/receitas', active='exact'),
+            ], vertical=True, pills=True, id='nav_bottons', style={'margin-bottom': '50px'}),
+            ], id='sidebar_completa')
+            ], style={'margin-top': '20px'}
+        )
+
+
 
 #========CALLBACKS
-
-#callback modal opcao1
+#callback modal receita
 @app.callback(
         Output('modal-botao1', 'is_open'),
         Input('botao-receita', 'n_clicks'),
@@ -84,7 +130,8 @@ layout = dbc.Card([
 def toggle_modal(n1, is_open):
     if n1: 
         return not is_open
-    
+
+#callback modal medicamentos
 @app.callback(
     Output('modal-botao2', 'is_open'),
     Input('botao-medicamentos', 'n_clicks'),
